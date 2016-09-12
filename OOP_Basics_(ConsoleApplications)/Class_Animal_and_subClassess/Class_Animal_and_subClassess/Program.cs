004a8ff91fcc1cb9578f1b97a9922563fcc7c721
maxeleron <maxeleron@gmail.com>
2016-09-12 18:23:05 +0300

    Created class Animal
    - Created Object Sharick

---
+++ OOP_Basics_(ConsoleApplications)/Class_Animal_and_subClassess/Class_Animal_and_subClassess/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Animal_and_subClassess
+{
+    class Animal
+    {
+        private bool isAlive = default(bool);
+        public bool GetIsAlive()
+        {
+            return isAlive;
+        }
+        public void SetIsAlive(bool value)
+        {
+            isAlive = value;
+        }
+        
+
+    }
+
+    class Chlenistonogie
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal sharick = new Animal();
+            sharick.SetIsAlive(false);
+            Console.WriteLine(sharick.GetIsAlive());
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
